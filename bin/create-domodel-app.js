737fcba6d5347d252a9728da74e6c5332675ae6e
thoughtsunificator <oss@thoughtsunificator.me>
2021-05-31 01:03:36 +0200

    clean up .git folder

--- bin/create-domodel-app.js
+++ bin/create-domodel-app.js
@@ -13,13 +13,15 @@ if(args.length === 0) {
 } else {
 	const name = args[0]
 	try {
-		await git().silent(false)	.clone("https://github.com/thoughtsunificator/domodel-skeleton", name)
+		await git().silent(false).clone("https://github.com/thoughtsunificator/domodel-skeleton", name)
 		const projectPath = path.resolve(process.cwd(), name)
 		const packagePath = path.resolve(projectPath, 'package.json')
 		const data = fs.readFileSync(packagePath)
 		let { devDependencies, dependencies, scripts } = JSON.parse(data);
+		fs.rmSync(path.resolve(projectPath, ".git"), { recursive: true })
 		fs.writeFileSync(packagePath, JSON.stringify({ name, devDependencies, dependencies, scripts }, null, "\t"));
 		execSync(`cd ${projectPath} && npm install`, {stdio: 'inherit'})
+		console.log("\n\u001b[32mYour domodel application is now ready. Happy codding!\u001b[0m")
 	} catch(ex) {
 		console.error('failed: ', ex)
 	}
